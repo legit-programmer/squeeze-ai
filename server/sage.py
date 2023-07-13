b97f859da6b76c3ee16fe74bf3747cca9be30298
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-07-13 18:06:47 +0530

    fixed: session timeout

--- server/sage.py
+++ server/sage.py
@@ -5,29 +5,38 @@ import json
 load_dotenv()
 
 token = os.getenv('TOKEN')
-client = poe.Client(token=token)
+client = None
+
+def ClientInit():
+    global client
+    client = poe.Client(token=token)
 
 
 def sendMessage(_message):
+    ClientInit()
     for chunk in client.send_message("chinchilla", _message):
         pass
     return chunk["text"]
 
 
 def getMessage(_prompt: str):
+    ClientInit()
     message = f'summarize a web page at {_prompt}'
     return sendMessage(message)
 
 
 def describeMessage():
+    ClientInit()
     message = 'I need a bit lengthy'
     return sendMessage(message)
 
 
 def cutMessage():
+    ClientInit()
     message = 'Shorten the informtaion please...'
     return sendMessage(message)
 
 
 print('Available bots:')
+ClientInit()
 print(json.dumps(client.bot_names, indent=4))
